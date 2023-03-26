e66d8c55e4d9921089ee998f22126075ac207b60
Luyee Wong <wongluyee1015@hotmail.com>
2023-03-26 17:16:15 +0900

    change validates wl_list_id

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,5 +4,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list, foreign_key: 'wl_list_id'
 
   validates :comment, length: { minimum: 6 }
-  validates :wl_movie_id, uniqueness: { scope: :list_id }
+  validates :wl_movie_id, uniqueness: { scope: :wl_list_id }
 end
